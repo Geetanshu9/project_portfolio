ca517266493e4393c7e0f56e9ef8787555fb271b
Geetanshu9 <101512109+Geetanshu9@users.noreply.github.com>
2023-09-03 21:36:36 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $('.btn-menu').click(function(){
+//         $(".header").toggleClass("active");
+//         console.log("dddd");
+//     })
+// })
+
+$(document).ready(function () {
+    $(".btn-menu").click(function(){
+        $(".header").toggleClass("active");
+        console.log("dddd");
+    })
+});
+
+
+console.log("sdds");
